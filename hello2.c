bc950e87d35e6c6d2b93ecddbf70ae688deab9e2
tp2smith <smith.taylor052@gmail.com>
2021-01-16 13:42:42 -0500

    test2

--- hello2.c
+++ hello2.c
@@ -2,14 +2,14 @@
 
 /*function definition*/
 
-int sum (int i, int j) {
-    int val = i+j;
+int sum (int i, int j, int k) {
+    int val = i+j+k;
     return val;
 }
 
 int main ()
 {
-    int x = 2, y = 3;
-    int su  = sum(x,y);
+    int x = 2, y = 3, z = 5;
+    int su  = sum(x,y,z);
     printf("sum is %d\n", su);
 }
